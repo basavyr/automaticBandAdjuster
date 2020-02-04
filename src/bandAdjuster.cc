4013b3f2a49c63f40d1f812c736f3df076f17f4e
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-02-04 18:21:09 +0200

    adds the band adjuster methods

--- src/bandAdjuster.cc
+++ src/bandAdjuster.cc
@@ -1,6 +1,7 @@
 #include "../include/bandAdjuster.hh"
 #include <iostream>
 
+//BENCHMARK TOOL
 TestApp::TestApp()
 {
     std::cout << "App is working fine..."
@@ -10,11 +11,17 @@ TestApp::TestApp()
 }
 TestApp::~TestApp()
 {
+    //if the creation failed...stop measuring time
+    if (!std::chrono::time_point_cast<std::chrono::microseconds>(startPoint).time_since_epoch().count())
+    {
+        std::cout << "Class container stopped with status FAIL"
+                  << "\n";
+        return;
+    }
     std::cout << "Class container stopped with status OK"
               << "\n";
+    //calls the method for getting the duration
     StopApp();
-    // std::cout << "Class container stopped with status FAIL"
-    //   << "\n";
 }
 
 void TestApp::RunApp()
@@ -37,4 +44,47 @@ void TestApp::StopApp()
     auto durationMS = (end - start) * 0.001;
     std::cout << "and was alive for " << durationMS << " ms..."
               << "\n";
+}
+
+BandAdjuster::BandAdjuster()
+{
+    const std::string message = "The class container for adjusting experimental band has been CREATED...\n";
+    std::cout << message;
+}
+
+BandAdjuster::~BandAdjuster()
+{
+    const std::string message = "The class container for adjusting experimental band has been DESTROYED...\n";
+    std::cout << message;
+}
+
+double BandAdjuster::BandSubtraction(std::vector<double> &array, double subtractor)
+{
+    return 0;
+}
+
+BandAdjuster::halfSize_tuple BandAdjuster::averageDeviation(std::vector<double> &array)
+{
+    auto avg_result = new halfSize_tuple();
+    auto halfSize = static_cast<size_t>(array.size() / 2);
+
+    std::cout << "full size" << array.size() << "\n";
+    std::cout << "half size" << halfSize << "\n";
+    for (int i = 0; i < halfSize; ++i)
+    {
+        avg_result->avg_Left += array.at(i);
+        avg_result->states_Left++;
+    }
+    avg_result->avg_Left = static_cast<double>(avg_result->avg_Left / avg_result->states_Left);
+    for (int i = halfSize; i < array.size(); ++i)
+    {
+        avg_result->avg_Right += array.at(i);
+        avg_result->states_Right++;
+    }
+    if (halfSize % 2 == 0)
+        avg_result->states_Right = avg_result->states_Right - 1;
+    std::cout << "states left " << avg_result->states_Left << "\n";
+    std::cout << "states right " << avg_result->states_Right << "\n";
+    avg_result->avg_Right = static_cast<double>(avg_result->avg_Right / avg_result->states_Right);
+    return *avg_result;
 }
--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,33 @@
 #include "../include/bandAdjuster.hh"
 
+void YRAST()
+{
+    auto adjuster = std::make_unique<BandAdjuster>();
+    auto data = std::make_unique<Data_ENSDF>();
+    auto x = adjuster->DELTA_Calculation(data->yrastExp, adjuster->band1Th_test);
+    for (auto &&n : x)
+    {
+        // std::cout << n << " , ";
+    }
+    auto xx = adjuster->averageDeviation(x);
+    adjuster->tuplePrinter(xx);
+}
+
+void WOBBLING()
+{
+    auto adjuster = std::make_unique<BandAdjuster>();
+    auto data = std::make_unique<Data_ENSDF>();
+    auto x = adjuster->DELTA_Calculation(data->wobbExp, adjuster->band2Th_test);
+    for (auto &&n : x)
+    {
+        // std::cout << n << " , ";
+    }
+    auto xx = adjuster->averageDeviation(x);
+    adjuster->tuplePrinter(xx);
+}
+
 int main()
 {
-    //benchmark tool
-    auto test = std::make_unique<TestApp>();
+    YRAST();
+    WOBBLING();
 }
